0eee799dd27149e343e71ae2bb29fa1a365aedc5
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-01-16 17:25:05 +0100

    move timezone

--- hosts/mandelBrut/configuration.nix
+++ hosts/mandelBrut/configuration.nix
@@ -1,42 +1,23 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
-      ../../modules/audio
-      ../../modules/nvidia
+      ../../sys-modules/audio
+      ../../sys-modules/nvidia
+      ../../sys-modules/timezone/fr
     ];
 
   environment.pathsToLink = ["/libexec"];
  
-
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "mandelBrut"; # Define your hostname.
   networking.networkmanager.enable = true;
 
-    # Set your time zone.
-  time.timeZone = "Europe/Paris";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-  };
 
   # Configure keymap in X11
   services.xserver = {
@@ -142,5 +123,7 @@ services.tumbler.enable = true; # Thumbnail support for images
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
