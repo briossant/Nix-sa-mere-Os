ffaefc47fe619a4a930eb9c13f9d24458aa50e2b
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-02-03 21:21:51 +0100

    add pulseeffect presets

--- home-modules/audio-utils/default.nix
+++ home-modules/audio-utils/default.nix
@@ -6,4 +6,9 @@
   home.packages = with pkgs; [
     pulsemixer # cli mixer 
   ];
+
+  home.file = {
+    ".config/PulseEffects/output/Basic Bass boost + reverb.json".source =
+      ./pulseeffect-presets/${"Basic Bass boost + reverb.json"};
+  };
 }
