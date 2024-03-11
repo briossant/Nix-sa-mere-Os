b6d8b1595f945ffba388744324efef4c4347d822
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-03-11 06:40:21 +0100

    wallpaper

--- home-modules/i3/default.nix
+++ home-modules/i3/default.nix
@@ -27,15 +27,38 @@ in
         "${mod}+Shift+j" = "move down";
         "${mod}+Shift+k" = "move up";
         "${mod}+Shift+l" = "move right";
+
+        "XF86AudioMute" = "exec amixer set Master toggle";
+        "XF86AudioLowerVolume" = "exec amixer set Master 4%-";
+        "XF86AudioRaiseVolume" = "exec amixer set Master 4%+";
+        "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
+        "XF86MonBrightnessUp" = "exec brightnessctl set 4%+";
+        "${mod}+Shift+x" = "exec systemctl suspend";
       };
 
       terminal = globalVars.terminal;
 
+      startup = [
+        {
+          command = "exec i3-msg workspace 1";
+          always = true;
+          notification = false;
+        }
+        {
+          command = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ~/wallpapers/*.JPG";
+          always = true;
+          notification = false;
+        }
+      ];
+
     };
     extraConfig = ''
       client.focused ${mkColor thm.main}       
+      client.focused_inactive ${mkColor thm.secondary}       
+      client.placeholder  ${mkColor thm.warning}        
       client.urgent  ${mkColor thm.warning}        
       client.unfocused ${mkColor thm.off}
+      client.background ${thm.extra.bg}
     '';
   };
 }
