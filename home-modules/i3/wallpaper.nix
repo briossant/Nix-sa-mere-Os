d5c1587bd8a29d4d3d9322c6594bb853dd89ccf1
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-03-18 17:17:08 +0100

    running a deamon is not easy

--- home-modules/i3/wallpaper.nix
+++ home-modules/i3/wallpaper.nix
@@ -1,24 +1,30 @@
 { config, lib, pkgs, globalVars, ... }:
 
 {
-  programs.script.changeWallpaper = {
-    enable = true;
-    content = ''
-      #!/usr/bin/env bash
-      ${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ~/wallpapers/*.JPG
-    '';
-  };
 
   systemd.user.services.wallpaper-changer = {
-    enable = true;
-    description = "Wallpaper Changer";
-    after = [ "graphical.target" ];
-    wantedBy = [ "graphical.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.home-manager}/bin/changeWallpaper";
-      Restart = "always";
-      RestartSec = "60"; # Restart every minute
+    Unit = {
+      description = "Wallpaper Refresher";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ~/wallpapers/*.JPG";
+    };
+  };
+
+  systemd.user.timers.wallpaper-changer = {
+    Unit = {
+      description = "Wallpaper Cycler";
+      wants = [ "display-manager.service" ];
+      after = [ "graphical-session.target" ];
+    };
+    Install = {
+      wantedBy = [ "graphical-session.target" ];
+    };
+    Timer = {
+      Unit = "wallpaper-changer.service";
+      OnCalendar = "*-*-* *:*:00";
+      Persistent = true;
     };
   };
 }
